6c0984b8c80fa1918f97c42e4de5af5364c2e002
Adam Ahmetasevic <adamahmetasevic@gmail.com>
2024-08-18 14:26:27 -0500

    Update HideArms.cs

--- Assets/Scripts/Player/HideArms.cs
+++ Assets/Scripts/Player/HideArms.cs
@@ -1,205 +1,73 @@
 using UnityEngine;
+using System.Linq;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class HideArms : MonoBehaviour
 {
-    public static GameManager instance;
+    public Material hideMaterial;
 
-    public bool isHoldingGun = false;
-    private bool playerDetected = false;
-    private bool objectMadeBig = false;
+    private string[] armBoneNames = new string[] {
+        "bip R UpperArm",
+        "bip R Forearm",
+        "bip R Hand",
+        "bip R Finger0",
+        "bip R Finger1",
+        "bip R Finger11",
+        "bip R Finger01",
+        "bip L UpperArm",
+        "bip L Forearm",
+        "bip L Hand",
+        "bip L Finger0",
+        "bip L Finger1",
+        "bip L Finger11",
+        "bip L Finger01"
+    };
 
-    [SerializeField] private GameObject sizeGun;
-    [SerializeField] private GameObject gravityGun;
-    [SerializeField] private Transform gunParent; // Assign this to your camera or hand transform in the inspector
+    private SkinnedMeshRenderer[] renderers;
+    private Dictionary<SkinnedMeshRenderer, Material[]> originalMaterials = new Dictionary<SkinnedMeshRenderer, Material[]>();
 
-    [SerializeField] private GameObject crosshair;
-
-    private GameObject currentGun;
-    private Dictionary<int, GameObject> inventory;
-
-    private HideArms hideArmsScript;
-
-    void Awake()
+    private void Start()
     {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-            return;
-        }
+        renderers = GetComponentsInChildren<SkinnedMeshRenderer>();
 
-        InitializeInventory();
-        hideArmsScript = FindObjectOfType<HideArms>();
-    }
-
-    void Start()
-    {
-        // Hide crosshair at the start
-        if (crosshair != null)
+        // Store original materials
+        foreach (SkinnedMeshRenderer renderer in renderers)
         {
-            crosshair.SetActive(false);
+            originalMaterials[renderer] = renderer.materials;
         }
     }
 
-    void InitializeInventory()
+    public void ApplyHideMaterial()
     {
-        inventory = new Dictionary<int, GameObject>
+        foreach (SkinnedMeshRenderer renderer in renderers)
         {
-            { 1, sizeGun },
-            { 2, gravityGun }
-        };
+            Transform[] bones = renderer.bones;
+            int[] armBoneIndices = bones
+                .Select((b, i) => new { Bone = b, Index = i })
+                .Where(item => armBoneNames.Contains(item.Bone.name))
+                .Select(item => item.Index)
+                .ToArray();
 
-        // Set the initial parent and position/rotation for each gun
-        foreach (var gun in inventory.Values)
-        {
-            if (gun != null)
+            if (armBoneIndices.Length > 0)
             {
-                gun.transform.SetParent(gunParent, false); // Set parent without changing local transform
-                gun.SetActive(false);
-            }
-        }
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            EquipGun(1);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            EquipGun(2);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            UnequipGun();
-        }
-    }
-
-    public void EquipGun(int gunIndex)
-    {
-        if (inventory.TryGetValue(gunIndex, out GameObject gun))
-        {
-            UnequipGun(); // Unequip any currently held gun
-
-            currentGun = gun;
-            currentGun.SetActive(true);
-
-            if (crosshair != null)
-            {
-                crosshair.SetActive(true); // Show crosshair when holding a gun
-            }
-
-            if (hideArmsScript != null)
-            {
-                hideArmsScript.ApplyHideMaterial(); // Hide arms when holding a gun
-                isHoldingGun = true;
-            }
-        }
-    }
-
-    public void UnequipGun()
-    {
-        if (currentGun != null)
-        {
-            currentGun.SetActive(false);
-            currentGun = null;
-
-            if (crosshair != null)
-            {
-                crosshair.SetActive(false);
-            }
-
-            if (hideArmsScript != null)
-            {
-                hideArmsScript.RestoreOriginalMaterials();
-                isHoldingGun = false;
-            }
-        }
-    }
-
-    public void AlertAllEnemies()
-    {
-        objectMadeBig = true;
-        if (!playerDetected)
-        {
-            EnemyAI[] enemies = FindObjectsOfType<EnemyAI>();
-            foreach (EnemyAI enemy in enemies)
-            {
-                enemy.PlayerDetected();
-            }
-            playerDetected = true;
-        }
-    }
-
-    public void PlayerLost()
-    {
-        if (playerDetected && !objectMadeBig)
-        {
-            EnemyAI[] enemies = FindObjectsOfType<EnemyAI>();
-            foreach (EnemyAI enemy in enemies)
-            {
-                enemy.LostPlayer();
-            }
-            playerDetected = false;
-        }
-    }
-
-    public bool IsObjectMadeBig()
-    {
-        return objectMadeBig;
-    }
-
-    public void ResetLevel()
-    {
-        // Reset any necessary game state
-        isHoldingGun = false;
-        playerDetected = false;
-        objectMadeBig = false;
-
-        // Unequip the current gun
-        UnequipGun();
-
-        // Deactivate existing guns instead of destroying them
-        if (inventory != null)
-        {
-            foreach (var gun in inventory.Values)
-            {
-                if (gun != null)
+                Material[] materials = renderer.materials;
+                for (int i = 0; i < materials.Length; i++)
                 {
-                    gun.SetActive(false);
+                    materials[i] = hideMaterial;
                 }
+                renderer.materials = materials;
             }
         }
+    }
 
-        // Ensure arms are visible
-        if (hideArmsScript != null)
+    public void RestoreOriginalMaterials()
+    {
+        foreach (SkinnedMeshRenderer renderer in renderers)
         {
-            hideArmsScript.RestoreOriginalMaterials();
+            if (originalMaterials.ContainsKey(renderer))
+            {
+                renderer.materials = originalMaterials[renderer];
+            }
         }
-
-        // Hide crosshair
-        if (crosshair != null)
-        {
-            crosshair.SetActive(false);
-        }
-
-        // Ensure gunParent is hidden
-        if (gunParent != null)
-        {
-            gunParent.gameObject.SetActive(false);
-        }
-
-        // Reset the current gun reference
-        currentGun = null;
-
-        // Reload the current scene
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
